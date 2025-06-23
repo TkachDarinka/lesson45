59cac424a72ea73820579a5ff10b7ec58dd1969b
Daria Tkach <tkachdarinka@gmail.com>
2025-06-23 21:59:49 +0200

    added tests:testValidPassword, testSymbolsLessThen8, invalidSpecSymbol,testNoUpperCase

--- src/validator/tests/PasswordValidatorTest.java
+++ src/validator/tests/PasswordValidatorTest.java
@@ -1,5 +1,35 @@
 package validator.tests;
+import org.junit.jupiter.api.Test;
+import validator.utils.PasswordValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class PasswordValidatorTest {
+
+    @Test
+    void testValidPassword(){
+        assertTrue(PasswordValidator.isStrong("Qwerty0!"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0?"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0@"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0#"));
+    }
+
+    @Test
+    void testSymbolsLessThen8(){
+    assertFalse((PasswordValidator.isStrong("Qwet0!")));
+    }
+
+    @Test
+    void invalidSpecSymbol(){
+        assertFalse(PasswordValidator.isStrong("Qwerty0 ;"));
+        assertFalse(PasswordValidator.isStrong("Qwe^rty0 ;"));
+        assertFalse(PasswordValidator.isStrong("~Qwerty0 ;"));
+    }
+
+    @Test
+    void testNoUpperCase(){
+        assertFalse(PasswordValidator.isStrong("qwerty2!"));
+    }
 
 }
